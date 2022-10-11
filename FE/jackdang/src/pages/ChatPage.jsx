6821b38f5fb0f404ddc62ba14b5cde69597c95d4
sang-rak <sangrack114@naver.com>
2022-10-11 23:05:40 +0900

    [FE/Ch-1] feat: 채팅페이지 하단 바 개발

---
+++ FE/jackdang/src/pages/ChatPage.jsx
@@ -0,0 +1,13 @@
+import Chat from "../components/Chat";
+import Footer from "../components/common/Footer";
+
+const ChatPage = () => {
+  return (
+    <div>
+      <Chat />
+      <Footer />
+    </div>
+  );
+};
+
+export default ChatPage;
