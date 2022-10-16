d6dc548d79b4080be75419488401c454c9852f06
sang-rak <sangrack114@naver.com>
2022-10-16 18:59:00 +0900

    [FE/My-2] feat: 마이페이지 편집 개발

--- FE/jackdang/src/components/Account/index.jsx
+++ FE/jackdang/src/components/Account/index.jsx
@@ -2,8 +2,13 @@ import { memo, useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
 import axios from "../../api/axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
-import { Col, Image, Row } from "react-bootstrap";
+import {
+  faChevronDown,
+  faPenToSquare,
+  faX,
+} from "@fortawesome/free-solid-svg-icons";
+import { Button, Col, Form, Image, Row } from "react-bootstrap";
+
 const Account = () => {
   const Account_URL = "";
   const [pagestatus, setPagestatus] = useState(""); // 화면 상태 저장
@@ -17,17 +22,22 @@ const Account = () => {
   const [address, setAddress] = useState("");
   const [age, setAge] = useState("");
   const [gender, setGender] = useState("");
+  const [job, setJob] = useState("");
+  const [school, setSchool] = useState("");
+  const [mbti, setMbti] = useState("");
+  const [love_status, setLove_status] = useState("");
+  const [religion, setReligion] = useState("");
   const [introduce, setIntroduce] = useState("안녕하세요");
   const [nickname, setNickname] = useState("");
-
+  const [editUse, setEditUse] = useState(true); // 편집아이콘 여부
   // 관심사 리스트
   // const [users, setUsers] = useState([]);
 
-  // 리랜더링되면 실행 되는 코드
+  // 리랜더링되면 실행
   useEffect(() => {
     fetchUserData();
   }, []);
-  // 회원 확인 코드
+  // 회원 확인
   const fetchUserData = async () => {
     // 추후 개발
     try {
@@ -46,9 +56,71 @@ const Account = () => {
       alert("로그인 후 사용가능합니다.");
     }
   };
+  // 회원 변경 내용 저장
+  const editUserData = async () => {
+    try {
+      // 세션 회원 확인
+      const id = "1";
+      const response = await axios.put(`/api/v1/member/${id}`, {
+        //보내고자 하는 데이터
+        age: age,
+      });
+      console.log(response);
+
+      setPagestatus("추가정보화면");
+    } catch (error) {
+      // 응답 실패 (로그아웃상태)
+      console.error(error);
+      alert("로그인 후 사용가능합니다.");
+    }
+  };
   const AccountPagePlus = async () => {
     // 추가 정보 더보기
     setPagestatus("추가정보화면");
+  };
+  /*
+   * 마이페이지 편집 로직
+   */
+  // 나이변경
+  const handleChangeAge = ({ target: { value } }) => {
+    setAge(value);
+  };
+  // 주소변경
+  const handleChangeAddress = ({ target: { value } }) => {
+    setAddress(value);
+  };
+  // 직업변경
+  const handleChangeJob = ({ target: { value } }) => {
+    setJob(value);
+  };
+  // 학력변경
+  const handleChangeSchool = ({ target: { value } }) => {
+    setSchool(value);
+  };
+  // MBTI 변경
+  const handleChangeMbti = ({ target: { value } }) => {
+    setMbti(value);
+  };
+  // 연애상태변경
+  const handleChangeLove_status = ({ target: { value } }) => {
+    setLove_status(value);
+  };
+  // 종교변경
+  const handleChangeReligion = ({ target: { value } }) => {
+    setReligion(value);
+  };
+
+  const AccountEditBtnEvent = async () => {
+    // 편집화면이 아니라면
+    if (pagestatus !== "정보편집화면") {
+      // 편집 페이지 전환
+      setEditUse(false);
+      setPagestatus("정보편집화면");
+    } else {
+      // 편집화면이 아니라면 화면 초기화
+      setEditUse(true);
+      setPagestatus("");
+    }
   };
 
   // 추가 정보 바뀌는 화면 설정
@@ -62,15 +134,15 @@ const Account = () => {
                 직업
               </Col>
               <Col xs={9} sm={9}>
-                취준생
+                {job}
               </Col>
             </Row>
             <Row>
               <Col xs={3} sm={3} className="text-primary">
-                종교
+                학교
               </Col>
               <Col xs={9} sm={9}>
-                불교
+                {school}
               </Col>
             </Row>
             <Row>
@@ -78,7 +150,7 @@ const Account = () => {
                 MBTI
               </Col>
               <Col xs={9} sm={9}>
-                INTP
+                {mbti}
               </Col>
             </Row>
             <Row>
@@ -86,12 +158,127 @@ const Account = () => {
                 연애
               </Col>
               <Col xs={9} sm={9}>
-                솔로
+                {love_status}
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={3} sm={3} className="text-primary">
+                종교
+              </Col>
+              <Col xs={9} sm={9}>
+                {religion}
               </Col>
             </Row>
           </>
         );
-
+      case "정보편집화면":
+        return (
+          <>
+            <Row>
+              <Col xs={3} sm={3} className="text-primary">
+                나이
+              </Col>
+              <Col xs={9} sm={9}>
+                <Form.Control
+                  type="age"
+                  name="age"
+                  value={age}
+                  onChange={handleChangeAge}
+                  placeholder="나이"
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={3} sm={3} className="text-primary">
+                지역
+              </Col>
+              <Col xs={9} sm={9}>
+                <Form.Control
+                  type="address"
+                  name="address"
+                  value={address}
+                  onChange={handleChangeAddress}
+                  placeholder="지역"
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={3} sm={3} className="text-primary">
+                직업
+              </Col>
+              <Col xs={9} sm={9}>
+                <Form.Control
+                  type="job"
+                  name="job"
+                  value={job}
+                  onChange={handleChangeJob}
+                  placeholder="직업"
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={3} sm={3} className="text-primary">
+                학교
+              </Col>
+              <Col xs={9} sm={9}>
+                <Form.Control
+                  type="school"
+                  name="school"
+                  value={school}
+                  onChange={handleChangeSchool}
+                  placeholder="학교"
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={3} sm={3} className="text-primary">
+                MBTI
+              </Col>
+              <Col xs={9} sm={9}>
+                <Form.Control
+                  type="mbti"
+                  name="mbti"
+                  value={mbti}
+                  onChange={handleChangeMbti}
+                  placeholder="MBTI"
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={3} sm={3} className="text-primary">
+                연애
+              </Col>
+              <Col xs={9} sm={9}>
+                <Form.Control
+                  type="love_status"
+                  name="love_status"
+                  value={love_status}
+                  onChange={handleChangeLove_status}
+                  placeholder="연애"
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={3} sm={3} className="text-primary">
+                종교
+              </Col>
+              <Col xs={9} sm={9}>
+                <Form.Control
+                  type="religion"
+                  name="religion"
+                  value={religion}
+                  onChange={handleChangeReligion}
+                  placeholder="종교"
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col className="text-center">
+                <Button onClick={editUserData}>저장하기</Button>
+              </Col>
+            </Row>
+          </>
+        );
       default:
         return (
           <Row onClick={AccountPagePlus}>
@@ -109,6 +296,14 @@ const Account = () => {
 
   return (
     <Container className="d-grid gap-2">
+      <Row className="my-3">
+        <Col className="text-end">
+          <FontAwesomeIcon
+            icon={editUse ? faPenToSquare : faX}
+            onClick={AccountEditBtnEvent}
+          />
+        </Col>
+      </Row>
       <Row className="mx-5 my-3">
         <Image
           roundedCircle
