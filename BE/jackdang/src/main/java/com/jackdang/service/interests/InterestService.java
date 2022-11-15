9fccd57cb995355cdf76776fac33525447824d8d
sang-rak <sangrack114@naver.com>
2022-11-15 22:47:37 +0900

    [BE/My-2] feat: 마이페이지 관심사 삭제

--- BE/jackdang/src/main/java/com/jackdang/service/interests/InterestService.java
+++ BE/jackdang/src/main/java/com/jackdang/service/interests/InterestService.java
@@ -25,11 +25,20 @@ public class InterestService {
 	public Long save(InterestDto interestDto) {
 		Long interestId = interestRepository.save(interestDto.toEntity()).getId();
 		Interest interest = interestRepository.findById(interestId).orElseThrow(() -> new IllegalArgumentException("해당 관심사가 없습니다"));
-		interest.update(interestDto.getInterest_nm(), interestDto.getMemberId());
+		interest.update(interestDto.getInterestNm(), interestDto.getMemberId());
 		return interestDto.getId(); 
 	}
+	 
+	/*
+	 *  관심사 삭제
+	 */
+	@Transactional
+	public void delete(Long memberId, String interestNm) {
+	    interestRepository.delete(
+	            interestRepository.findByMember_idAndInterestNm(memberId, interestNm).orElseThrow(() -> new IllegalArgumentException("해당 관심사가 없습니다"))
+	    );
+	}
 	
-
 
 	// 관심사 전체 조회
 	public List<Interest> findInterests() {
